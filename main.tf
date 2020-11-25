8a5603498ed0a34665ec9201251fbf2b4fba1f8b
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-25 14:10:05 -0600

    Added a new alarm

--- main.tf
+++ main.tf
@@ -58,3 +58,17 @@ module "instances" {
   vpc_id                   = module.network.VPC_ID
   subnet_id                = module.network.SUBNET_ID
 }
+
+# Create the alarm for CPU utilization
+resource "aws_cloudwatch_metric_alarm" "foobar" {
+  alarm_name                = "terraform-test-foobar5"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
