825a7861255b6a2e51acf6164a915e25f4f20c1d
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-25 14:42:10 -0600

    Added sns topic

--- main.tf
+++ main.tf
@@ -60,7 +60,6 @@ module "instances" {
   subnet_id                = module.network.SUBNET_ID
 }
 
-<<<<<<< HEAD
 # Setup SNS topic for alerting
 resource "aws_sns_topic" "alarm" {
   name = "alarms-topic"
@@ -87,18 +86,4 @@ EOF
   provisioner "local-exec" {
     command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.alarms_email}"
   }
-=======
-# Create the alarm for CPU utilization
-resource "aws_cloudwatch_metric_alarm" "foobar" {
-  alarm_name                = "terraform-test-foobar5"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = "120"
-  statistic                 = "Average"
-  threshold                 = "80"
-  alarm_description         = "This metric monitors ec2 cpu utilization"
-  insufficient_data_actions = []
->>>>>>> main
 }
