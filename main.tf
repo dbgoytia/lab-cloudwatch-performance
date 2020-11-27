30e9d32f5e4492d9b1ab1b8a92f4cba81d4d46a9
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-26 19:07:42 -0600

    Testing sns integration

--- main.tf
+++ main.tf
@@ -72,3 +72,30 @@ resource "aws_cloudwatch_metric_alarm" "foobar" {
   alarm_description         = "This metric monitors ec2 cpu utilization"
   insufficient_data_actions = []
 }
+
+resource "aws_sns_topic" "alarm" {
+  name = "alarms-topic"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+
+  provisioner "local-exec" {
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.alarms_email}"
+  }
+}
